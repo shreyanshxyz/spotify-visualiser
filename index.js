9d0afdab33910a9742bd4c3b03bf00237b25b720
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-26 19:31:18 +0530

    update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 8888;
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
 app.get("/", (req, res) => {
   res.json("It's Working");
 });
