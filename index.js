46bf38f49a5088a2b77dd42ad6cbf09798624321
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-26 19:37:57 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const querystring = require("querystring");
 const express = require("express");
 const app = express();
 
@@ -12,12 +13,27 @@ app.get("/", (req, res) => {
   res.json("It's Working");
 });
 
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = "spotify_auth_state";
+
 app.get("/login", (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
 
   const scope = "user-read-private user-read-email";
 
+  //   client id, response_type & redirect_uri are all required params for us to login, state & scope are optional
+  //   We turn all of them into a combined string
+  //   The Query String module provides a way of parsing the URL query string.
   const queryParams = querystring.stringify({
     client_id: CLIENT_ID,
     response_type: "code",
