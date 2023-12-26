d1fea25333b08066a846b0194e39431d8c584f5e
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-26 20:54:28 +0530

    Final Commit

--- client/src/styles/GlobalStyle.js
+++ client/src/styles/GlobalStyle.js
@@ -1,10 +1,79 @@
 import { createGlobalStyle } from 'styled-components/macro';
-import fonts from './fonts';
-import variables from './variables';
+import theme from './theme';
+const { colors, fontSizes, fonts } = theme;
 
 const GlobalStyle = createGlobalStyle`
-  ${fonts};
-  ${variables};
+  @font-face {
+    font-family: 'Circular Std';
+    src: local('Circular Std Medium'), local('CircularStd-Medium'),
+    url('../fonts/CircularStd-Medium.woff2') format('woff2'),
+    url('../fonts/CircularStd-Medium.woff') format('woff');
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Circular Std';
+    src: local('Circular Std Book'), local('CircularStd-Book'),
+    url('../fonts/CircularStd-Book.woff2') format('woff2'),
+    url('../fonts/CircularStd-Book.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Circular Std';
+    src: local('Circular Std Medium Italic'), local('CircularStd-MediumItalic'),
+    url('../fonts/CircularStd-MediumItalic.woff2') format('woff2'),
+    url('../fonts/CircularStd-MediumItalic.woff') format('woff');
+    font-weight: 500;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'Circular Std';
+    src: local('Circular Std Black'), local('CircularStd-Black'),
+    url('../fonts/CircularStd-Black.woff2') format('woff2'),
+    url('../fonts/CircularStd-Black.woff') format('woff');
+    font-weight: 900;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Circular Std';
+    src: local('Circular Std Bold'), local('CircularStd-Bold'),
+    url('../fonts/CircularStd-Bold.woff2') format('woff2'),
+    url('../fonts/CircularStd-Bold.woff') format('woff');
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Circular Std';
+    src: local('Circular Std Bold Italic'), local('CircularStd-BoldItalic'),
+    url('../fonts/CircularStd-BoldItalic.woff2') format('woff2'),
+    url('../fonts/CircularStd-BoldItalic.woff') format('woff');
+    font-weight: 700;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'Circular Std';
+    src: local('Circular Std Book Italic'), local('CircularStd-BookItalic'),
+    url('../fonts/CircularStd-BookItalic.woff2') format('woff2'),
+    url('../fonts/CircularStd-BookItalic.woff') format('woff');
+    font-weight: 400;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'Circular Std';
+    src: local('Circular Std Black Italic'), local('CircularStd-BlackItalic'),
+    url('../fonts/CircularStd-BlackItalic.woff2') format('woff2'),
+    url('../fonts/CircularStd-BlackItalic.woff') format('woff');
+    font-weight: 900;
+    font-style: italic;
+  }
 
   html {
     box-sizing: border-box;
@@ -16,61 +85,55 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: inherit;
   }
 
+  html,
   body {
     margin: 0;
     padding: 0;
     width: 100%;
     max-width: 100%;
+  }
+
+  body {
     min-height: 100%;
     overflow-x: hidden;
     -moz-osx-font-smoothing: grayscale;
     -webkit-font-smoothing: antialiased;
-    background-color: var(--black);
-    color: var(--white);
-    font-family: var(--font);
-    font-size: var(--fz-md);
+    font-family: ${fonts.primary};
+    font-size: ${fontSizes.base};
+    background-color: ${colors.black};
+    color: ${colors.white};
+  }
+
+  #root {
+    min-height: 100%;
   }
 
   h1, h2, h3, h4, h5, h6 {
-    letter-spacing: -.04em;
+    letter-spacing: -.025em;
     margin: 0 0 10px;
+    font-weight: 700;
+  }
+
+  h1, h2, h3 {
+    font-weight: 900;
   }
 
   p {
     margin: 0 0 10px;
   }
 
-  a,
-  button {
-    transition: all 0.3s ease;
-    color: inherit;
+  ol, ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
   }
 
   a {
+    display: inline-block;
     text-decoration: none;
-
-    &:hover,
-    &:focus {
-      text-decoration: underline;
-    }
-  }
-
-  button {
-    border: 0;
+    color: inherit;
+    transition: ${theme.transition};
     cursor: pointer;
-    font-family: inherit;
-    border-radius: var(--border-radius-pill);
-    background-color: rgba(0,0,0,.7);
-    color: var(--white);
-    font-size: var(--fz-sm);
-    font-weight: 700;
-    padding: var(--spacing-xs) var(--spacing-sm);
-
-    &:hover,
-    &:focus {
-      background-color: var(--dark-grey);
-      outline: 0;
-    }
   }
 
   img {
@@ -79,42 +142,42 @@ const GlobalStyle = createGlobalStyle`
     vertical-align: middle;
   }
 
-  main {
-    position: relative;
-    padding: var(--spacing-xxl) 0;
+  svg {
+    fill: currentColor;
+    vertical-align: middle;
   }
 
-  .app {
-    min-height: 100vh;
+  input {
+    border-radius: 0;
+    outline: 0;
+    &::placeholder {
+      opacity: 0.7;
+    }
+    &:focus,
+    &:active {
+      &::placeholder {
+        opacity: 0.5;
+      }
+    }
   }
 
-  .sr-only {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    margin: -1px;
-    overflow: hidden;
-    clip: rect(0, 0, 0, 0);
-    white-space: nowrap;
+  button {
+    display: inline-block;
+    color: ${colors.lightestGrey};
+    font-family: ${fonts.primary};
+    font-size: ${fontSizes.base};
+    font-weight: 700;
+    border-radius: 50px;
     border: 0;
-  }
+    padding: 10px 20px;
+    cursor: pointer;
+    transition: ${theme.transition};
 
-  .overflow-ellipsis {
-    -webkit-line-clamp: 1;
-    -webkit-box-orient: vertical;
-    display: -webkit-box;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: unset;
-    word-break: break-all;
-  }
-
-  .empty-notice {
-    color: var(--grey);
-    font-size: var(--fz-lg);
-    text-align: center;
-    padding: var(--spacing-xxl);
+    &:hover,
+    &:focus {
+      color: ${colors.white};
+      outline: 0;
+    }
   }
 `;
 
--- server/index.js
+++ server/index.js
@@ -1,134 +1,182 @@
-require("dotenv").config();
-const querystring = require("querystring");
-const express = require("express");
-const axios = require("axios");
-const app = express();
+// SPOTIFY WEB API AUTHORIZATION CODE FLOW
+// https://developer.spotify.com/documentation/general/guides/authorization-guide/
+// https://github.com/spotify/web-api-auth-examples
 
-const port = 8888;
+require('dotenv').config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
+let REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8888/callback';
+let FRONTEND_URI = process.env.FRONTEND_URI || 'http://localhost:3000';
+const PORT = process.env.PORT || 8888;
 
-app.get("/", (req, res) => {
-  res.json("It's Working");
-});
+if (process.env.NODE_ENV !== 'production') {
+  REDIRECT_URI = 'http://localhost:8888/callback';
+  FRONTEND_URI = 'http://localhost:3000';
+}
 
-const generateRandomString = (length) => {
-  let text = "";
-  const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const express = require('express');
+const request = require('request');
+const cors = require('cors');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const history = require('connect-history-api-fallback');
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = length => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
 };
 
-const stateKey = "spotify_auth_state";
+const stateKey = 'spotify_auth_state';
 
-app.get("/login", (req, res) => {
-  const state = generateRandomString(16);
-  res.cookie(stateKey, state);
+// Multi-process to utilize all CPU cores.
+if (cluster.isMaster) {
+  console.warn(`Node cluster master ${process.pid} is running`);
 
-  const scope = "user-read-private user-read-email";
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-  //   client id, response_type & redirect_uri are all required params for us to login, state & scope are optional
-  //   We turn all of them into a combined string
-  //   The Query String module provides a way of parsing the URL query string.
-  const queryParams = querystring.stringify({
-    client_id: CLIENT_ID,
-    response_type: "code",
-    redirect_uri: REDIRECT_URI,
-    state: state,
-    scope: scope,
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`,
+    );
+  });
+} else {
+  const app = express();
+
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+  app
+    .use(express.static(path.resolve(__dirname, '../client/build')))
+    .use(cors())
+    .use(cookieParser())
+    .use(
+      history({
+        verbose: true,
+        rewrites: [
+          { from: /\/login/, to: '/login' },
+          { from: /\/callback/, to: '/callback' },
+          { from: /\/refresh_token/, to: '/refresh_token' },
+        ],
+      }),
+    )
+    .use(express.static(path.resolve(__dirname, '../client/build')));
+
+  app.get('/', function (req, res) {
+    res.render(path.resolve(__dirname, '../client/build/index.html'));
   });
 
-  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
-});
+  app.get('/login', function (req, res) {
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
 
-app.get("/callback", (req, res) => {
-  // Our code queryparam
-  const code = req.query.code || null;
+    // your application requests authorization
+    const scope =
+      'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public';
 
-  axios({
-    method: "post",
-    url: "https://accounts.spotify.com/api/token",
-    // Stringifying 3 required params
-    data: querystring.stringify({
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: REDIRECT_URI,
-    }),
-    // Request headers
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${new Buffer.from(
-        `${CLIENT_ID}:${CLIENT_SECRET}`
-      ).toString("base64")}`,
-    },
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        // const { access_token, token_type } = response.data;
+    res.redirect(
+      `https://accounts.spotify.com/authorize?${querystring.stringify({
+        response_type: 'code',
+        client_id: CLIENT_ID,
+        scope: scope,
+        redirect_uri: REDIRECT_URI,
+        state: state,
+      })}`,
+    );
+  });
 
-        // axios.get('https://api.spotify.com/v1/me', {
-        //   headers: {
-        //     Authorization: `${token_type} ${access_token}`
-        //   }
-        // })
-        //   .then(response => {
-        //     res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-        //   })
-        //   .catch(error => {
-        //     res.send(error);
-        //   });
+  app.get('/callback', function (req, res) {
+    // your application requests refresh and access tokens
+    // after checking the state parameter
 
-        const { refresh_token } = response.data;
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
 
-        axios
-          .get(
-            `http://localhost:8888/refresh_token?refresh_token=${refresh_token}`
-          )
-          .then((response) => {
-            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-          })
-          .catch((error) => {
-            res.send(error);
-          });
-      } else {
-        res.send(response);
+    if (state === null || state !== storedState) {
+      res.redirect(`/#${querystring.stringify({ error: 'state_mismatch' })}`);
+    } else {
+      res.clearCookie(stateKey);
+      const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: REDIRECT_URI,
+          grant_type: 'authorization_code',
+        },
+        headers: {
+          Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+            'base64',
+          )}`,
+        },
+        json: true,
+      };
+
+      request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          const access_token = body.access_token;
+          const refresh_token = body.refresh_token;
+
+          // we can also pass the token to the browser to make requests from there
+          res.redirect(
+            `${FRONTEND_URI}/#${querystring.stringify({
+              access_token,
+              refresh_token,
+            })}`,
+          );
+        } else {
+          res.redirect(`/#${querystring.stringify({ error: 'invalid_token' })}`);
+        }
+      });
+    }
+  });
+
+  app.get('/refresh_token', function (req, res) {
+    // requesting access token from refresh token
+    const refresh_token = req.query.refresh_token;
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+          'base64',
+        )}`,
+      },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token,
+      },
+      json: true,
+    };
+
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const access_token = body.access_token;
+        res.send({ access_token });
       }
-    })
-    .catch((error) => {
-      res.send(error);
     });
-});
+  });
 
-app.get("/refresh_token", (req, res) => {
-  const { refresh_token } = req.query;
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
+  });
 
-  axios({
-    method: "post",
-    url: "https://accounts.spotify.com/api/token",
-    data: querystring.stringify({
-      grant_type: "refresh_token",
-      refresh_token: refresh_token,
-    }),
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${new Buffer.from(
-        `${CLIENT_ID}:${CLIENT_SECRET}`
-      ).toString("base64")}`,
-    },
-  })
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((error) => {
-      res.send(error);
-    });
-});
-
-app.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`);
-});
+  app.listen(PORT, function () {
+    console.warn(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+  });
+}
